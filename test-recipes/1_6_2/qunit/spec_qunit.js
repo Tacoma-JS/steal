a6a81f099f3756f7e75ee29a658c9bf24a04ac29
NorthDecoder <joe@vividventures.biz>
2017-12-06 20:10:44 +0000

    Add test for steal.loader.allowModuleExecution

--- test-recipes/1_6_2/qunit/spec_qunit.js
+++ test-recipes/1_6_2/qunit/spec_qunit.js
@@ -1,8 +1,16 @@
 /*global QUnit steal*/
 
 /**
-   Usage:
-         See the QUnit [api](http://api.qunitjs.com/)
+ * QUnit test specifications to help gain understanding of the steal.js methods
+ * and properties.  You can do something like `console.dir(steal)` to
+ * display these in alphabetical order.  The tests are in the same order.
+ * 
+ * Passing test do NOT mean that `steal` is functioning as expected, it only
+ * means that the tests were written retrospectively to pass the way it is
+ * working, which may be different than the expectation.
+ * 
+ * QUnit Usage:
+ *       See the [api](http://api.qunitjs.com/)
 */
 
 var moduleName = "spec_qunit.js";
@@ -33,15 +41,29 @@ var test01 = function (argument) {
     // ES6 shortcut for QUnit.test, Qunit.skip etc
     const { test, skip, module, todo } = QUnit;
 
+
+
     /* * * * * * * * * * * * * * * * * * * * * * * * * */
     module( "steal" );
+    /* * * * * * * * * * * * * * * * * * * * * * * * * */
+
       steal.dev.log(`  steal function:`);
       console.dir(steal);
 
-    todo( "version under test === steal.version", function( assert ) {
+    /**
+     * Steal version 1.6.2 under test === steal.version
+     * from https://cdn.jsdelivr.net/npm/steal@1.6.2/steal.js
+     * (This property does not exist, yet.)
+    */
+    todo( "version 1.6.2 under test === steal.version . From"+
+          " https://cdn.jsdelivr.net/npm/steal@1.6.2/steal.js",
+      function( assert ) {
         assert.equal( steal.version, "1.6.2","Expecting version 1.6.2" );
     });
 
+    /**
+     * `steal` is a function that has sixteen properties and methods.
+    */
     test("is 1) a function that 2) has sixteen properties and methods.",
       function( assert ) {
         assert.equal(typeof(steal),'function',"Is a function.");
@@ -49,8 +71,12 @@ var test01 = function (argument) {
         assert.equal(pmNames.length,16,"Has sixteen properties and methods");
     });
 
+    /**
+     * FYI `steal.loader` object methods and properties are exactly similar
+     * to the `steal.System` object
+    */
     test("FYI `steal.loader` object methods and properties are exactly similar"+
-         " to `steal.System` object",
+         " to the `steal.System` object",
       function( assert ) {
         var arry1 = Object.keys(steal.loader);//array of property & method names
         var arry2 = Object.keys(steal.System);//array of property & method names
@@ -59,9 +85,14 @@ var test01 = function (argument) {
     });
 
 
+
     /* * * * * * * * * * * * * * * * * * * * * * * * * */
     module( "System" );
+    /* * * * * * * * * * * * * * * * * * * * * * * * * */
 
+    /**
+     * `System`is an object that has fifty five properties and methods.
+    */
     test("is 1) an object that 2) has fifty five properties and methods.",
       function( assert ) {
         assert.equal(typeof(System),'object',"Is an object");
@@ -69,6 +100,9 @@ var test01 = function (argument) {
         assert.equal(pmNames.length,55,"Expecting 55 properties and methods");
     });
 
+    /**
+     * `System`can also be called like `steal.System`.
+    */
     test("can also be called like steal.System",
       function( assert ) {
         assert.equal(typeof(steal.System),'object',"It is an object");
@@ -77,30 +111,36 @@ var test01 = function (argument) {
     });
 
 
+
     /* * * * * * * * * * * * * * * * * * * * * * * * * */
-    module( "steal.loader method" );
+    module( "steal.loader METHOD" );
+    /* * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    test("steal.loader.allowModuleExecution(name) expects a valid module name"+
-         " as input and outputs an object promise ",
+
+/**     `steal.loader.allowModuleExecution(name)` expects a valid string
+        in `name` as input and returns an object promise that notifies when
+        the property steal.loader._allowModuleExecution[name] is set.
+*/
+    test("`steal.loader.allowModuleExecution(name)` expects a valid string "+
+        "in `name` as input and returns an object promise that notifies when "+
+        "the property steal.loader._allowModuleExecution[name] is set.",
       function( assert ) {
-        var name = "try_any_valid_string";
+        var name = "try_any_valid_string"; // only testing correct usage
         var okToExecute = steal.loader.allowModuleExecution(name);
-        assert.equal( typeof(okToExecute),'object',"The output is an object {Promise}." );
-        console.log( "  okToExecute" );
-        console.dir( okToExecute );
 
-        console.log( "  okToExecute.then()" );
-        okToExecute.then(
-          function(value) { console.log("    resolved: " + value);},
-          function(value) { console.log("    rejected: " + value);}
-        );
+        assert.equal( typeof(okToExecute),
+                      'object',"Returns an object {Promise}." );
 
-        assert.equal( okToExecute._handler.resolved,true,"It is okay to"+
-                    " execute the valid module named with: " + name );
-    });
-
-
-
+        return okToExecute.then(
+            function(value) {
+              assert.equal( steal.loader._allowModuleExecution[name],true,
+                    "It is okay to "+
+                    "execute the valid module named with: " + name );
+            },
+          // should never fail in this test
+          function(value) {console.log("okToExecute.then() rejected: "+value); }
+        );// end then
+    }); // end test steal.loader.allowModuleExecution(name)
 
 
 }; //end function Test01
